6e7870677198b4bec6ad7c6c70bef4f84ddcb903
charumeyn <charcandava@gmail.com>
2023-05-28 10:19:24 +0900

    feat: [Auth] UserAuth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,11 @@ import { SheltersModule } from './shelters/shelters.module';
 import { FundraisersModule } from './fundraisers/fundraisers.module';
 import { AddressesModule } from './addresses/addresses.module';
 import { PostsModule } from './posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
+import { getEnvPath } from './common/helper/env.helper';
+import { UserAuthModule } from './user-auth/user-auth.module';
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
   imports: [DogsModule, SheltersModule, FundraisersModule, AddressesModule, PostsModule, TypeOrmModule.forRoot({
@@ -15,7 +20,8 @@ import { PostsModule } from './posts/posts.module';
     autoLoadEntities: true,
     migrations: ['dist/db/migrations/*.js'],
     synchronize: true,
-  })],
+  }), UserAuthModule,
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user-auth/user/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthController } from './auth.controller';
+import { AuthHelper } from './auth.helper';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './auth.strategy';
+import { ConfigService } from '@nestjs/config';
+import { User } from '../user.entity';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt', property: 'user' }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        // secret: config.get('JWT_KEY'),
+        // signOptions: { expiresIn: config.get('JWT_EXPIRES') },
+        secret: 'TEST_KEY',
+        signOptions: { expiresIn: '365d' },
+      }),
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, AuthHelper, JwtStrategy],
+})
+export class AuthModule { }
